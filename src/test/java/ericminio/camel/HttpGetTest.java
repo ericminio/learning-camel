a1af1281da6774880d8c0b1d00ca7d46af1ea822
Eric Mignot <eric.mignot@gmail.com>
2018-05-04 09:34:11 -0700

    camel can force 404-Not found

--- src/test/java/ericminio/camel/HttpGetTest.java
+++ src/test/java/ericminio/camel/HttpGetTest.java
@@ -2,6 +2,7 @@ package ericminio.camel;
 
 import ericminio.support.HttpResponse;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.junit.After;
@@ -32,8 +33,8 @@ public class HttpGetTest {
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("jetty:http://localhost:8888/greeting")
-                        .setBody(constant("Hello World"));
+            from("jetty:http://localhost:8888/greeting")
+                .setBody(constant("Hello World"));
             }
         });
         HttpResponse response = get( "http://localhost:8888/greeting" );
@@ -51,13 +52,30 @@ public class HttpGetTest {
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("jetty:http://localhost:8888/greeting")
-                        .process(exchange -> { call.wasMade = true; } )
-                        .setBody(constant("Hello World"));
+            from("jetty:http://localhost:8888/greeting")
+                .process(exchange -> { call.wasMade = true; } )
+                .setBody(constant("Hello World"));
             }
         });
         get( "http://localhost:8888/greeting" );
 
         assertThat(call.wasMade, equalTo(true));
+    }
+
+    @Test
+    public void camelCanForceNotFound() throws Exception {
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+            from("jetty:http://localhost:8888/greeting")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(404))
+                .setBody(simple("NOT FOUND"))
+            ;
+            }
+        });
+        HttpResponse response = get( "http://localhost:8888/greeting" );
+
+        assertThat(response.getStatusCode(), equalTo(404 ));
+        assertThat(response.getBody(), equalTo("NOT FOUND" ));
     }
 }
--- src/test/java/ericminio/support/GetRequest.java
+++ src/test/java/ericminio/support/GetRequest.java
@@ -11,13 +11,16 @@ public class GetRequest {
         HttpURLConnection request = (HttpURLConnection) new URL( url ).openConnection();
         HttpResponse response = new HttpResponse();
         response.setStatusCode(request.getResponseCode());
-        response.setBody(getResponseBody(request));
+        if (request.getResponseCode() < 400) {
+            response.setBody(getResponseBody(request.getInputStream()));
+        } else {
+            response.setBody(getResponseBody(request.getErrorStream()));
+        }
 
         return response;
     }
 
-    protected static String getResponseBody(HttpURLConnection request) throws IOException {
-        InputStream inputStream = request.getInputStream();
+    protected static String getResponseBody(InputStream inputStream) throws IOException {
         byte[] response = new byte[ inputStream.available() ];
         inputStream.read( response );
         return new String(response);
